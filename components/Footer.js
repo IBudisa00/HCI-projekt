eef481af984aced150360f7a6e422b1f56e07668
IBudisa00 <73231557+IBudisa00@users.noreply.github.com>
2022-02-09 12:32:23 +0100

    Add files via upload

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+const Footer = () => {
+    return (
+      <footer className="flex xl:absolute justify-center pt-10 bg-black text-white text-center w-screen bottom-0  h-20">
+                @FESB 
+            HCI Project Karla i Ivan
+        </footer>
+    )
+}
+
+export default Footer;
